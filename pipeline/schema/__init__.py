5efccbd11d1e3bbc703f631780b13256ecc86250
diosamuel <koding12sam@gmail.com>
2026-06-18 08:30:11 +0700

    add: schema init.py

--- pipeline/schema/__init__.py
+++ pipeline/schema/__init__.py
@@ -1,7 +1,6 @@
 from schema.listSchema import (
     default_schema,
-    default_schema_enrichment,
-    sitasi_schema,
+    sitasi_schema
 )
 
-__all__ = ["default_schema", "default_schema_enrichment", "sitasi_schema"]
+__all__ = ["default_schema", "sitasi_schema"]
